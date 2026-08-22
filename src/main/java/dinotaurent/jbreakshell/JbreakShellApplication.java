7e565ea398d0a396855e287ee723bf3e7ea4d95e
Dinotaurent <danieldazawow@gmail.com>
2026-08-22 15:31:08 -0500

    Inicio

---
+++ src/main/java/dinotaurent/jbreakshell/JbreakShellApplication.java
@@ -0,0 +1,13 @@
+package dinotaurent.jbreakshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbreakShellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbreakShellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dinotaurent/jbreakshell/JbreakShellApplicationTests.java
@@ -0,0 +1,13 @@
+package dinotaurent.jbreakshell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbreakShellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
