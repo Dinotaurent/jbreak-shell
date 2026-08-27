09ca5aa40f860b9c8bbf09ad158a304f86a61704
Dinotaurent <danieldazawow@gmail.com>
2026-08-27 15:11:11 -0500

    metodo play() funcionando

--- src/main/java/dinotaurent/jbreakshell/service/BreakService.java
+++ src/main/java/dinotaurent/jbreakshell/service/BreakService.java
@@ -4,8 +4,16 @@ import dinotaurent.jbreakshell.dto.BreakStatus;
 import org.jline.terminal.Attributes;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.Terminal.SignalHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.time.Duration;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
@@ -18,6 +26,12 @@ public class BreakService {
     private int descansosCompletados = 0;
     private int descansosSaltados = 0;
     private int contadorDescansos = 0;
+
+    @Value("classpath:alert.wav")
+    private Resource sonido1;
+
+    @Value("classpath:alert2.wav")
+    private Resource sonido2;
 
     // Hilo actual, guardarlo es necesario para poderlo interrumpirlo.
     private volatile Thread timerThread = null;
@@ -59,6 +73,7 @@ public class BreakService {
             IO.println("[i] Presiona CTRL + C en cualquier momento para cancelar.");
 
             contar(2);
+            play(1);
 
             // --- En construccion ---
             tiempoEnPc = tiempoEnPc.plus(Duration.ofMinutes(30));
@@ -70,7 +85,7 @@ public class BreakService {
             // ---
         } catch (InterruptedException e){
             IO.println("\n[✖] Temporizador cancelado.");
-
+            play(2);
             return Optional.empty();
         } finally {
             // Se limpia la referencia del hilo al terminar correctamente o interrumpirse.
@@ -99,5 +114,25 @@ public class BreakService {
 
     private void contar(int minutos) throws InterruptedException{
         TimeUnit.MINUTES.sleep(minutos);
+    }
+
+    private void play(int opcion){
+        BufferedInputStream reader;
+        try {
+            if (opcion == 1){
+                reader = new BufferedInputStream(sonido1.getInputStream());
+            } else {
+                reader = new BufferedInputStream(sonido2.getInputStream());
+            }
+
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(reader);
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+
+        } catch (Exception e){
+            IO.println("Se produjo un error: " + e.getMessage());
+        }
     }
 }
